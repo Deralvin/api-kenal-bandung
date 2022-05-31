a4cf44ae3f8c46793c06369748368cadfa9752e3
jganz6 <jodiemanopo28@gmail.com>
2022-06-01 06:31:02 +0700

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/* eslint-disable require/first */
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const { socketConnection } = require("./socket.js");
+const http = require("http");
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.raw());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT;
+
+const server = http.createServer(app);
+const socketOptions = {
+  cors: {
+    origin: "*",
+  },
+};
+server.listen(port, () => {
+  console.log("Server Running at Port", port);
+});
+socketConnection(server, socketOptions);
